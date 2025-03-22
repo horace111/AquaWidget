4b8ae174099f032cc08590d37d8a9eb8b07a6324
MnlSmile <kedoukedou33@163.com>
2025-03-22 18:50:33 +0800

    nothing important

--- widgets/weather.py
+++ widgets/weather.py
@@ -12,16 +12,18 @@ def set_acquire_func(func) -> None:
 def acquire() -> QWidget:
     return acquire_func()
 
-def get_weather():
-    pass
-'''
-weather -- request from server
-'''
+def get_weather_by_local_ip():
+    r = requests.get('http://47.119.20.145:82/v1/weather/ip_based_quick_query')
+    '''
+    weather -- request from server
+    '''
+    return r.json()
 
 def reg() -> AquaWidget:
     global acquire_func
     aquaw = AquaWidget(scale = (370, 370))
-    degree = 15
-    show_degree = QLabel('{degree}°C'.format(degree = degree), parent = aquaw.get_aqua_widget())
+    show_degree = QLabel(parent = aquaw.get_aqua_widget())
+    weather = get_weather_by_local_ip()['data']
+    show_degree.setText(f"{weather['cityId']} {weather['temperature']}℃")
     show_degree.move(15, 15)
     return aquaw
